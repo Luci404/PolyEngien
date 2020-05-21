d9336979cdb83e073b8bcb2483560b1f95fab34d
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-05-21 23:07:51 +0200

    Shader abstraction

--- PolyEngien/src/PolyEngien/Renderer/Renderer.cpp
+++ PolyEngien/src/PolyEngien/Renderer/Renderer.cpp
@@ -1,7 +1,10 @@
 #include "pepch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 namespace PolyEngien {
+
 	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
 
 	void Renderer::BeginScene(OrthographicCamera& camera)
@@ -13,13 +16,14 @@ namespace PolyEngien {
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform) 
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
 	}
+
 }
--- PolyViewer/src/PolyViewerApp.cpp
+++ PolyViewer/src/PolyViewerApp.cpp
@@ -1,8 +1,11 @@
 #include <PolyEngien.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public PolyEngien::Layer
 {
@@ -86,9 +89,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new PolyEngien::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(PolyEngien::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -105,20 +108,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
+			uniform vec3 u_Color;
 
 			in vec3 v_Position;
 
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1);
 			}
 		)";
 
-		m_BlueShader.reset(new PolyEngien::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(PolyEngien::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(PolyEngien::Timestep ts) override
@@ -147,13 +152,17 @@ public:
 		PolyEngien::Renderer::BeginScene(m_Camera);
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		std::dynamic_pointer_cast<PolyEngien::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<PolyEngien::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				PolyEngien::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				PolyEngien::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -164,19 +173,20 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(PolyEngien::Event& event) override
 	{
-
 	}
 
 private:
 	std::shared_ptr<PolyEngien::Shader> m_Shader;
 	std::shared_ptr<PolyEngien::VertexArray> m_VertexArray;
 
-	std::shared_ptr<PolyEngien::Shader> m_BlueShader;
+	std::shared_ptr<PolyEngien::Shader> m_FlatColorShader;
 	std::shared_ptr<PolyEngien::VertexArray> m_SquareVA;
 
 	PolyEngien::OrthographicCamera m_Camera;
@@ -185,6 +195,8 @@ private:
 	
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class PolyViewer : public PolyEngien::Application
