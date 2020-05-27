53f63f6038da973a2fced792266a6418f5463978
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-05-27 14:29:52 +0200

    Basic ref-counting system to terminate glfw

--- PolyEngien/src/Platform/Windows/WindowsWindow.cpp
+++ PolyEngien/src/Platform/Windows/WindowsWindow.cpp
@@ -9,7 +9,7 @@
 
 namespace PolyEngien {
 
-	static bool s_GLFWInitialized = false;
+	static uint8_t s_GLFWWindowCount = 0;
 
 	static void GLFWErrorCallback(int error, const char* description)
 	{
@@ -39,19 +39,19 @@ namespace PolyEngien {
 
 		PE_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWWindowCount == 0) 
 		{
-			// TODO: glfwTerminate on system shutdown
+			PE_CORE_INFO("Initializing GLFW");
 			int success = glfwInit();
 			PE_CORE_ASSERT(success, "Could not intialize GLFW!");
 
 			glfwSetErrorCallback(GLFWErrorCallback);
 
-			s_GLFWInitialized = true;
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		
+		++s_GLFWWindowCount;
+
 		m_Context = CreateScope<OpenGLContext>(m_Window);
 		m_Context->Init();
 
@@ -151,6 +151,13 @@ namespace PolyEngien {
 	void WindowsWindow::Shutdown()
 	{
 		glfwDestroyWindow(m_Window);
+
+		s_GLFWWindowCount -= 1;
+			if (s_GLFWWindowCount == 0)
+		{
+			PE_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 	void WindowsWindow::OnUpdate()
