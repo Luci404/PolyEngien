bf0d8cbbf56dde1b5a4b552655b0816af331b027
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-05-27 14:32:34 +0200

    Fix Build warnings to do with BufferElement Offset Type

--- PolyEngien/src/PolyEngien/Renderer/Buffer.h
+++ PolyEngien/src/PolyEngien/Renderer/Buffer.h
@@ -33,10 +33,10 @@ namespace PolyEngien {
 		std::string Name;
 		ShaderDataType Type;
 		uint32_t Size;
-		uint32_t Offset;
+		size_t Offset;
 		bool Normalized;
 
-		BufferElement() {}
+		BufferElement() = default;
 
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
 			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
@@ -86,8 +86,9 @@ namespace PolyEngien {
 	private:
 		void CalculateOffsetsAndStride()
 		{
-			uint32_t offset = 0;
+			size_t offset = 0;
 			m_Stride = 0;
+
 			for (auto& element : m_Elements)
 			{
 				element.Offset = offset;
