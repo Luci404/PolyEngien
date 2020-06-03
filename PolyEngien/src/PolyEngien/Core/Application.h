fa330f6ceb772629a1a32f166beea0ed50164fa8
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-06-03 19:58:00 +0200

    make runloop only accessible on the engine side

--- PolyEngien/src/PolyEngien/Core/Application.h
+++ PolyEngien/src/PolyEngien/Core/Application.h
@@ -11,6 +11,8 @@
 
 #include "PolyEngien/imgui/ImGuiLayer.h"
 
+int main(int argc, char** argv);
+
 namespace PolyEngien {
 
 	class Application
@@ -18,8 +20,6 @@ namespace PolyEngien {
 	public:
 		Application();
 		virtual ~Application();
-
-		void Run();
 
 		void OnEvent(Event & e);
 
@@ -30,6 +30,7 @@ namespace PolyEngien {
 
 		inline static Application& Get() { return *s_Instance; }
 	private:
+		void Run();
 		bool OnWindowClose(WindowCloseEvent & e);
 		bool OnWindowResize(WindowResizeEvent& e);
 	private:
@@ -41,6 +42,7 @@ namespace PolyEngien {
 		float m_LastFrameTime = 0.0f;
 	private:
 		static Application* s_Instance;
+		friend int ::main(int argc, char** argv);
 	};
 
 	// To be defined in client
