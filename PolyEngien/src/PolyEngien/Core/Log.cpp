c5940b57e7e08dcf8168200f3b86395a709cd59e
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-06-04 15:04:32 +0200

    Log messages are now saved to .pelog file

--- PolyEngien/src/PolyEngien/Core/Log.cpp
+++ PolyEngien/src/PolyEngien/Core/Log.cpp
@@ -1,7 +1,8 @@
 #include "pepch.h"
 #include "PolyEngien/Core/Log.h"
 
-//#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace PolyEngien {
 
@@ -10,12 +11,29 @@ namespace PolyEngien {
 	
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");	// -startcolorr(%^) [Timestamp([%T)] nameoflogger(core/client)(%n): message(%v) -endcolor(%$)
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("PolyEngien.pelog", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("POLYENGIEN", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
+
+		/*spdlog::set_pattern("%^[%T] %n: %v%$");	// -startcolorr(%^) [Timestamp([%T)] nameoflogger(core/client)(%n): message(%v) -endcolor(%$)
 		
 		s_CoreLogger = spdlog::stdout_color_mt("POLY ENGIEN");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ClientLogger = spdlog::stdout_color_mt("APPLICATION");
-		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->set_level(spdlog::level::trace);*/
 	}
 }
