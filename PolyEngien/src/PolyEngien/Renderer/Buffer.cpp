0a0e9584d67362d98a85dea6840c5b6a8118bb52
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-06-04 15:37:20 +0200

    Batch rendering

--- PolyEngien/src/PolyEngien/Renderer/Buffer.cpp
+++ PolyEngien/src/PolyEngien/Renderer/Buffer.cpp
@@ -6,8 +6,22 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace PolyEngien {
+
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    PE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		PE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
+
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    PE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
