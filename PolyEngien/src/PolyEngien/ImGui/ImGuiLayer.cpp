4877a8973113349029126c9f143d33452c69765b
Storm Soendergaard <stormsoendergaard@gmail.com>
2020-06-04 16:36:46 +0200

    Imgui handling mouse and keyboards events

--- PolyEngien/src/PolyEngien/ImGui/ImGuiLayer.cpp
+++ PolyEngien/src/PolyEngien/ImGui/ImGuiLayer.cpp
@@ -67,6 +67,13 @@ namespace PolyEngien {
 		ImGui::DestroyContext();
 	}
 
+	void ImGuiLayer::OnEvent(Event& e)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+	}
+
 	void ImGuiLayer::Begin()
 	{
 		PE_PROFILE_FUNCTION();
