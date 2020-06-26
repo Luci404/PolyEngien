c30a0a79b7469e369e4dae3f3a655412ca345792
Storm Søndergaard <stormsoendergaard@gmail.com>
2020-06-26 21:25:04 +0200

    Fixed compile error on previous commit

--- PolyEngien/src/PolyEngien/Renderer/GraphicsContext.h
+++ PolyEngien/src/PolyEngien/Renderer/GraphicsContext.h
@@ -5,7 +5,7 @@ namespace PolyEngien {
 	class GraphicsContext
 	{
 	public:
-		virtual ~GraphicsContext() = default
+		virtual ~GraphicsContext() = default;
 
 		virtual void Init() = 0;
 		virtual void SwapBuffers() = 0;
